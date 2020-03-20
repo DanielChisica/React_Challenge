9aca98451a286111d79b078c82f07683e9d15b53
DanielChisica <danielchisica777@gmail.com>
2020-03-20 05:09:56 -0500

    skeleton created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider} from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './redux/reducers/rootReducer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const store = createStore(rootReducer);
+
+class App extends React.Component {
+  render() {
+    return (
+        <Provider store={store}>
+        </Provider>
+    );
+  }
+};
 
 export default App;
